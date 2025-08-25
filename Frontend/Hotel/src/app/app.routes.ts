0ad15f1b47bb8fdaa7156b73891af19f9caad60a
Jefferson Quiroz <jeffersonquirozrodriguez@gmail.com>
2025-08-25 16:36:20 -0500

    home actualizado y login eliminado

--- Frontend/Hotel/src/app/app.routes.ts
+++ Frontend/Hotel/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { List_habitacion } from './component/habitacion/listar_habitacion/list_habitacion';
 import { Home } from './component/home/home';
 import { ReservaHabitacion } from './component/habitacion/reserva-habitacion/reserva-habitacion';
-import { Login } from './component/login/login';
+
 import { Contact } from './component/contact/contact';
 import { About } from './component/about/about';
 import { Privacy } from './component/layout/privacy/privacy';
@@ -13,8 +13,8 @@ export const routes: Routes = [
     {path: "", component: Home},
     {path: "habitaciones", component: List_habitacion},
     { path: 'reservar/:id', component: ReservaHabitacion},
-    { path: 'login', component: Login},
-    //nuevo:
+
+
     { path: 'contacto', component: Contact},
     { path: 'about', component:About },
     { path: 'privacy', component:Privacy},
--- Frontend/Hotel/src/app/component/home/home.ts
+++ Frontend/Hotel/src/app/component/home/home.ts
@@ -1,13 +1,33 @@
-  import { Component } from '@angular/core';
+  import { CommonModule } from '@angular/common';
+  import { Component, OnInit } from '@angular/core';
   import { RouterModule } from '@angular/router';
-
+  import { Habitacion } from '../../servicios/habitacion.service';
+  import { HabitacionService } from '../../servicios/habitacion.service';
   @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, CommonModule],
     templateUrl: './home.html',
 
   })
-  export class Home {
+export class Home implements OnInit {
+  habitaciones: Habitacion[] = [];
+  cargando = true;
+  error: string | null = null;
 
+  constructor(private habitacionService: HabitacionService) {}
+
+  ngOnInit(): void {
+    this.habitacionService.getHabitaciones().subscribe({
+      next: (data) => {
+        this.habitaciones = data.slice(0, 8); // 🔹 Solo las primeras 8
+        this.cargando = false;
+      },
+      error: (err) => {
+        console.error('Error cargando habitaciones', err);
+        this.error = 'No se pudieron cargar habitaciones populares';
+        this.cargando = false;
+      },
+    });
   }
+}
